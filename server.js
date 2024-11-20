36dbfe71bd0225fd0371a3f2e3bd7c1d9bf0ece2
Pranav2501 <96072640+Pranav2501@users.noreply.github.com>
2024-11-19 17:43:48 -0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files (like your CSS, JS, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
